c91e16e9b2bed23f7a06420ef45f153c212d6f91
estevaolibardi <estevaolibardi@hotmail.com>
2023-01-16 18:40:33 -0300

    feat: test

--- lektorr/components/page.jsx
+++ lektorr/components/page.jsx
@@ -6,17 +6,6 @@ function Page({ title, description, children }) {
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
-        <html lang="pt"></html>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lexend:wght@600&display=swap"
-          rel="stylesheet"
-        />
       </Head>
 
       {children}
